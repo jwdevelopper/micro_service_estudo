cf8274d0db7712fc9ab6b0e82c748d705186a46a
José Wilson Junior <jwdevelopper@gmail.com>
2022-01-19 12:30:23 -0300

    Inicio Impl

---
+++ crud/src/main/java/com/foxestudo/crud/entity/Produto.java
@@ -0,0 +1,26 @@
+package com.foxestudo.crud.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Produto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String nome;
+    @Column
+    private Integer estoque;
+    private Double preco;
+}
---
+++ crud/src/main/java/com/foxestudo/crud/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.foxestudo.crud.repository;
+
+import com.foxestudo.crud.entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
